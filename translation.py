3fd8168772f6654ccc1a86f469b177e18902b16e
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-04 01:51:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ class Translation(object):
     START_TEXT = """Hi {},
 I'm URL-Uploader!
 You can upload HTTP/HTTPS direct link, Using this bot!
-MainTain By @Bot4Universe
+MainTain By @TMWAD
 /help for more details!"""
     
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
