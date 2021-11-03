47c9901d488a2339b063a9604fd11a46496f1423
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-04 02:58:01 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | Modifieded By : HashMinner
 
 # the logging things
 import logging
@@ -8,113 +8,111 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+import random
 import numpy
 import os
 from PIL import Image
 import time
-
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
 # the Strings used for this "thing"
 from translation import Translation
-
-import pyrogram
+from pyrogram import Client as Clinton
+from database.access import clinton
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import filters
+from database.adduser import AddUser
+from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["genthumbnail"]))
-async def generate_custom_thumbnail(bot, update):
-    if update.from_user.id in Config.AUTH_USERS:
-        if update.reply_to_message is not None:
-            reply_message = update.reply_to_message
-            if reply_message.media_group_id is not None:
-                download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
-                save_final_image = download_location + str(round(time.time())) + ".jpg"
-                list_im = os.listdir(download_location)
-                if len(list_im) == 2:
-                    imgs = [ Image.open(download_location + i) for i in list_im ]
-                    inm_aesph = sorted([(numpy.sum(i.size), i.size) for i in imgs])
-                    min_shape = inm_aesph[1][1]
-                    imgs_comb = numpy.hstack(numpy.asarray(i.resize(min_shape)) for i in imgs)
-                    imgs_comb = Image.fromarray(imgs_comb)
-                    # combine: https://stackoverflow.com/a/30228789/4723940
-                    imgs_comb.save(save_final_image)
-                    # send
-                    user = await bot.get_me()
-                    mention = user["mention"]
-                    await bot.send_photo(
-                        chat_id=update.chat.id,
-                        photo=save_final_image,
-                        caption=Translation.CUSTOM_CAPTION_UL_FILE.format(mention),
-                        reply_to_message_id=update.message_id
-                    )
-                else:
-                    await bot.send_message(
-                        chat_id=update.chat.id,
-                        text=Translation.ERR_ONLY_TWO_MEDIA_IN_ALBUM,
-                        reply_to_message_id=update.message_id
-                    )
-                try:
-                    [os.remove(download_location + i) for i in list_im ]
-                    os.remove(download_location)
-                except:
-                    pass
-            else:
-                await bot.send_message(
-                    chat_id=update.chat.id,
-                    text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-                    reply_to_message_id=update.message_id
-                )
-        else:
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-                reply_to_message_id=update.message_id
-            )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.photo)
+@Clinton.on_message(filters.private & filters.photo)
 async def save_photo(bot, update):
-    if update.from_user.id in Config.AUTH_USERS:
-        if update.media_group_id is not None:
-            # album is sent
-            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
-            # create download directory, if not exist
-            if not os.path.isdir(download_location):
-                os.makedirs(download_location)
-            await bot.download_media(
-                message=update,
-                file_name=download_location
-            )
-        else:
-            # received single photo
-            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-            await bot.download_media(
-                message=update,
-                file_name=download_location
-            )
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text=Translation.SAVED_CUSTOM_THUMB_NAIL,
-                reply_to_message_id=update.message_id
-            )
+    await AddUser(bot, update)
+    await clinton.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
+    await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
 
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["delthumbnail"]))
+@Clinton.on_message(filters.private & filters.command("delthumbnail"))
 async def delthumbnail(bot, update):
-    if update.from_user.id in Config.AUTH_USERS:
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
-        try:
-            os.remove(download_location + ".jpg")
-            # os.remove(download_location + ".json")
-        except:
-            pass
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
-            reply_to_message_id=update.message_id
-        )
+    await AddUser(bot, update)
+    await clinton.set_thumbnail(update.from_user.id, thumbnail=None)
+    await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+
+@Clinton.on_message(filters.private & filters.command("viewthumbnail") )
+async def viewthumbnail(bot, update):
+    await AddUser(bot, update)
+    thumbnail = await clinton.get_thumbnail(update.from_user.id)
+    if thumbnail is not None:
+        await bot.send_photo(
+        chat_id=update.chat.id,
+        photo=thumbnail,
+        caption=f"Your current saved thumbnail ✅",
+        reply_to_message_id=update.message_id)
+    else:
+        await update.reply_text(text=f"No Thumbnail found 🤒")
+
+async def Gthumb01(bot, update):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await clinton.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        Image.open(thumbnail).convert("RGB").save(thumbnail)
+        img = Image.open(thumbnail)
+        img.resize((100, 100))
+        img.save(thumbnail, "JPEG")
+    else:
+        thumbnail = None
+
+    return thumbnail
+
+async def Gthumb02(bot, update, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await clinton.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+    else:
+        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
+
+    return thumbnail
+
+async def Mdata01(download_directory):
+
+          width = 0
+          height = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+              if metadata.has("height"):
+                  height = metadata.get("height")
+
+          return width, height, duration
+
+async def Mdata02(download_directory):
+
+          width = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+
+          return width, duration
+
+async def Mdata03(download_directory):
+
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+
+          return duration
